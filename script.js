945c580615b0c88ededa9baccdd164c30e6082d8
DorinSuciu <87894066+DorinSuciu@users.noreply.github.com>
2021-08-05 22:31:27 +0300

    Added the delete function (and it works)

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// function addUser() {
-
-//     let table = document.getElementById('user-table');
-//     let row = table.insertRow(-1);
-//     let nameCell = row.insertCell(0);
-//     let emailCell = row.insertCell(1);
-//     let sexCell = row.insertCell(2);
-//     let birthDateCell = row.insertCell(3);
+function onFormSubmit() {
+    var formData = readFormData();
+    insertNewRow(formData);
+}
 
 function readFormData() {
     var firstName = document.getElementById('first-name').value;
@@ -15,74 +11,34 @@ function readFormData() {
     var birthDate = document.getElementById('birth-date').value;
 
     var formData = {
-        userFullName: firstName + " " + lastName,
-        userEmail: email,
-        userSex: sex,
-        userBirthDate: birthDate
+        employeeFullName: firstName + " " + lastName,
+        employeeEmail: email,
+        employeeSex: sex,
+        employeeBirthDate: birthDate
     }
-
-    // formData.userFullName = document.getElementById('first-name').value + " " + document.getElementById('last-name').value;
-    // formData.userEmail = document.getElementById('email').value;
-    // formData.userSex = document.getElementById('sex-selector').value;
-    // formData.userBirthDate = document.getElementById('birth-date').value;
     return formData;
 }
 
+function deleteEmployee(td) {
 
 
-//     if (typeof firstName !== 'string' || firstName == "") {
-//         alert("Please provide your name!");
-
-//         return false;
-//     } else if (typeof lastName !== 'string' || lastName == "") {
-//         alert("Please provide your name!");
-
-//         return false;
-//     } else {
-//         nameCell += firstName + " " + lastName;
-//     }
-
-
-
-//     emailCell.innerHTML = email;
-//     sexCell.innerHTML = sex;
-//     birthDateCell.innerHTML = birthDate;
-// }
-
-var selectedRow = null;
-
-// let's validate some data
-
-// function validateData() {
-//     var isValid = true;
-//     // if (typeof firstName !== 'string') {
-//     //     isValid = false;
-//     //     alert("Please don't introduce numbers in your")
-//     // } else {
-//     //     isValid = true;
-//     // }
-//     return isValid;
-// }
-
-function onFormSubmit() {
-    // if (validateData()) {
-    var formData = readFormData();
-
-    insertNewRow(formData);
-
-
-    // }
+    if (confirm("Are you sure you want to delete this employee?")) {
+        row = td.parentElement.parentElement;
+        document.getElementById('user-table').deleteRow(row.rowIndex);
+    }
 }
 
 function insertNewRow(data) {
-    var table = document.getElementById('user-table').getElementsByTagName('tbody')[0];
-    var row = table.insertRow(-1);
+    let table = document.getElementById('user-table').getElementsByTagName('tbody')[0];
+    let row = table.insertRow(-1);
     let nameCell = row.insertCell(0);
-    nameCell.innerHTML = data.userFullName;
-    var emailCell = row.insertCell(1);
-    emailCell.innerHTML = data.userEmail;
-    var sexCell = row.insertCell(2);
-    sexCell.innerHTML = data.userSex;
-    var birthDateCell = row.insertCell(3);
-    birthDateCell.innerHTML = data.userBirthDate;
+    nameCell.innerHTML = data.employeeFullName;
+    let emailCell = row.insertCell(1);
+    emailCell.innerHTML = data.employeeEmail;
+    let sexCell = row.insertCell(2);
+    sexCell.innerHTML = data.employeeSex;
+    let birthDateCell = row.insertCell(3);
+    birthDateCell.innerHTML = data.employeeBirthDate;
+    let deleteCell = row.insertCell(4);
+    deleteCell.innerHTML = `<button onClick="deleteEmployee(this)">Delete</button>`
 }
