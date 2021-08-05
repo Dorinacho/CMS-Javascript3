b3accfef2a36bc7a7587ff18ea60ccb2c3836893
DorinSuciu <87894066+DorinSuciu@users.noreply.github.com>
2021-08-05 23:22:19 +0300

    Basic version - done

--- script.js
+++ script.js
@@ -8,28 +8,26 @@ function readFormData() {
     var lastName = document.getElementById('last-name').value;
     var email = document.getElementById('email').value;
     var sex = document.getElementById('sex-selector').value;
-    var birthDate = document.getElementById('birth-date').value;
+    var birthdate = document.getElementById('birthdate').value;
 
     var formData = {
         employeeFullName: firstName + " " + lastName,
         employeeEmail: email,
         employeeSex: sex,
-        employeeBirthDate: birthDate
+        employeeBirthdate: birthdate
     }
     return formData;
 }
 
 function deleteEmployee(td) {
-
-
     if (confirm("Are you sure you want to delete this employee?")) {
         row = td.parentElement.parentElement;
-        document.getElementById('user-table').deleteRow(row.rowIndex);
+        document.getElementById('employee-table').deleteRow(row.rowIndex);
     }
 }
 
 function insertNewRow(data) {
-    let table = document.getElementById('user-table').getElementsByTagName('tbody')[0];
+    let table = document.getElementById('employee-table').getElementsByTagName('tbody')[0];
     let row = table.insertRow(-1);
     let nameCell = row.insertCell(0);
     nameCell.innerHTML = data.employeeFullName;
@@ -37,8 +35,8 @@ function insertNewRow(data) {
     emailCell.innerHTML = data.employeeEmail;
     let sexCell = row.insertCell(2);
     sexCell.innerHTML = data.employeeSex;
-    let birthDateCell = row.insertCell(3);
-    birthDateCell.innerHTML = data.employeeBirthDate;
+    let birthdateCell = row.insertCell(3);
+    birthdateCell.innerHTML = data.employeeBirthdate;
     let deleteCell = row.insertCell(4);
     deleteCell.innerHTML = `<button onClick="deleteEmployee(this)">Delete</button>`
 }
